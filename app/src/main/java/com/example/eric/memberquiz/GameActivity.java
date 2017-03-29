b339c78f1515daa9408e180e36bbe51f2e9d2bd5
eric <eric.kong98@gmail.com>
2017-03-29 19:32:18 -0400

    Making the edits and changes

--- app/src/main/java/com/example/eric/memberquiz/GameActivity.java
+++ app/src/main/java/com/example/eric/memberquiz/GameActivity.java
@@ -15,8 +15,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Random;
@@ -31,100 +29,57 @@ public class GameActivity extends AppCompatActivity {
     Button endButton;
     TextView score;
     TextView countdownText;
+    ImageView image;
     SharedPreferences pref;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+    Random rand;
+    int rand_img_num;
+    String rand_img;
 
-        String[] members = {"Jessica Cherny", "Kevin Jiang", "Jared Gutierrez", "Kristin Ho", "Christine Munar", "Mudit Mittal", "Richard Hu", "Shaan Appel", "Edward Liu", "Wilbur Shi", "Young Lin", "Abhinav Koppu", "Abhishek Mangla", "Akkshay Khoslaa", "Andy Wang", "Aneesh Jindal", "Anisha Salunkhe", "Ashwin Vaidyanathan", "Cody Hsieh", "Justin Kim", "Krishnan Rajiyah", "Lisa Lee", "Peter Schafhalter", "Sahil Lamba", "Sirjan Kafle", "Tarun Khasnavis", "Billy Lu", "Aayush Tyagi", "Ben Goldberg", "Candice Ye", "Eliot Han", "Emaan Hariri", "Jessica Chen", "Katharine Jiang", "Kedar Thakkar", "Leon Kwak", "Mohit Katyal", "Rochelle Shen", "Sayan Paul", "Sharie Wang", "Shreya Reddy", "Shubham Goenka", "Victor Sun", "Vidya Ravikumar"};
-        ArrayList<String>image_names = new ArrayList<>();
-        Field[] drawables = com.example.eric.memberquiz.R.drawable.class.getFields();
-        for (Field f : drawables) {
-            try {
-                String name = f.getName();
-                if (!name.contains("_") && !name.contains("$") && !name.equals("serialVersionUID")){
-                    System.out.println(name);
-                    image_names.add(name);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println(image_names.size());
+    String wrong_name1;
+    String wrong_name2;
+    String wrong_name3;
 
-        // Get random image
-        Random rand = new Random();
-        int rand_img_num = rand.nextInt(44);
-        String rand_img = image_names.get(rand_img_num);
+    Button[]answer_buttons;
+
+    String[] members = {"Jessica Cherny", "Kevin Jiang", "Jared Gutierrez", "Kristin Ho", "Christine Munar", "Mudit Mittal", "Richard Hu", "Shaan Appel", "Edward Liu", "Wilbur Shi", "Young Lin", "Abhinav Koppu", "Abhishek Mangla", "Akkshay Khoslaa", "Andy Wang", "Aneesh Jindal", "Anisha Salunkhe", "Ashwin Vaidyanathan", "Cody Hsieh", "Justin Kim", "Krishnan Rajiyah", "Lisa Lee", "Peter Schafhalter", "Sahil Lamba", "Sirjan Kafle", "Tarun Khasnavis", "Billy Lu", "Aayush Tyagi", "Ben Goldberg", "Candice Ye", "Eliot Han", "Emaan Hariri", "Jessica Chen", "Katharine Jiang", "Kedar Thakkar", "Leon Kwak", "Mohit Katyal", "Rochelle Shen", "Sayan Paul", "Sharie Wang", "Shreya Reddy", "Shubham Goenka", "Victor Sun", "Vidya Ravikumar"};
+    ArrayList<String>image_names = new ArrayList<>();
+    Field[] drawables = com.example.eric.memberquiz.R.drawable.class.getFields();
+
+    public void loadGame(){
 
         // Setting the random image
-        ImageView image = (ImageView) findViewById(R.id.imageView);
+        getRandImage();
+
         int id = getApplicationContext().getResources().getIdentifier("drawable/"+rand_img, null, getApplicationContext().getPackageName());
         image.setImageResource(id);
 
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
-        button4 = (Button) findViewById(R.id.button4);
-        endButton = (Button) findViewById(R.id.endButton);
-        score = (TextView) findViewById(R.id.score);
-        countdownText = (TextView) findViewById(R.id.countdownText);
-        pref = getApplicationContext().getSharedPreferences("MyPref", 0);
-
         int new_score = pref.getInt("score", 0);
         score.setText(Integer.toString(new_score));
-
-        Button[]answer_buttons = new Button[4];
-        answer_buttons[0] = button1;
-        answer_buttons[1] = button2;
-        answer_buttons[2] = button3;
-        answer_buttons[3] = button4;
-
-        //Clear All Button Text
-        for(int i = 0; i < answer_buttons.length; i++){
-            answer_buttons[i].setText("");
-        }
 
         int rand_but_num = rand.nextInt(4);
 
 
         //Gets other 3 names randomly
-        int rand_name1 = rand.nextInt(44);
+        int rand_name1 = rand.nextInt(members.length);
         while (rand_name1 == rand_img_num){
-            rand_name1 = rand.nextInt(44);
+            rand_name1 = rand.nextInt(members.length);
         }
-        String wrong_name1 = image_names.get(rand_name1);
+        wrong_name1 = image_names.get(rand_name1);
 
-        int rand_name2 = rand.nextInt(44);
+        int rand_name2 = rand.nextInt(members.length);
         while (rand_name2 == rand_img_num || rand_name2 == rand_name1){
-            rand_name2 = rand.nextInt(44);
+            rand_name2 = rand.nextInt(members.length);
         }
-        String wrong_name2 = image_names.get(rand_name2);
+        wrong_name2 = image_names.get(rand_name2);
 
-        int rand_name3 = rand.nextInt(44);
+        int rand_name3 = rand.nextInt(members.length);
         while (rand_name3 == rand_img_num || rand_name3 == rand_name1 || rand_name3 == rand_name2){
-            rand_name3 = rand.nextInt(44);
+            rand_name3 = rand.nextInt(members.length);
         }
-        String wrong_name3 = image_names.get(rand_name3);
+        wrong_name3 = image_names.get(rand_name3);
 
-
-        // Gets properly formatted name of the Image
-        for(int i = 0; i < members.length; i++){
-            if(members[i].replaceAll("\\s+","").equalsIgnoreCase(rand_img)){
-                rand_img = members[i];
-            }
-            if(members[i].replaceAll("\\s+","").equalsIgnoreCase(wrong_name1)){
-                wrong_name1 = members[i];
-            }
-            if(members[i].replaceAll("\\s+","").equalsIgnoreCase(wrong_name2)){
-                wrong_name2 = members[i];
-            }
-            if(members[i].replaceAll("\\s+","").equalsIgnoreCase(wrong_name3)){
-                wrong_name3 = members[i];
-            }
-        }
+        formatNames();
 
         String[]button_options = new String[3];
         button_options[0] = wrong_name1;
@@ -140,7 +95,6 @@ public class GameActivity extends AppCompatActivity {
             }
         }
 
-        //Count Down Timer
         timer = new CountDownTimer(5500, 1000) {
 
             public void onTick(long millisUntilFinished) {
@@ -150,20 +104,92 @@ public class GameActivity extends AppCompatActivity {
             public void onFinish() {
                 countdownText.setText("Times Up!");
                 if (countdownText.getText().toString().equals("Times Up!")) {
-                    finish();
-                    startActivity(getIntent());
+                    loadGame();
                 }
             }
-        }.start();
+        };
+        timer.start();
+
+    }
+
+    public void getRandImage(){
+        // Get random image
+        rand = new Random();
+        rand_img_num = rand.nextInt(members.length);
+        rand_img = image_names.get(rand_img_num);
+
+    }
+
+    public void formatNames(){
+        // Gets properly formatted name of the Image
+        for(int i = 0; i < members.length; i++){
+            if(members[i].replaceAll("\\s+","").equalsIgnoreCase(rand_img)){
+                rand_img = members[i];
+            }
+            if(members[i].replaceAll("\\s+","").equalsIgnoreCase(wrong_name1)){
+                wrong_name1 = members[i];
+            }
+            if(members[i].replaceAll("\\s+","").equalsIgnoreCase(wrong_name2)){
+                wrong_name2 = members[i];
+            }
+            if(members[i].replaceAll("\\s+","").equalsIgnoreCase(wrong_name3)){
+                wrong_name3 = members[i];
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+        for (Field f : drawables) {
+            try {
+                String name = f.getName();
+                if (!name.contains("_") && !name.contains("$") && !name.equals("serialVersionUID")){
+                    System.out.println(name);
+                    image_names.add(name);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(image_names.size());
+
+        image = (ImageView) findViewById(R.id.imageView);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        endButton = (Button) findViewById(R.id.endButton);
+        score = (TextView) findViewById(R.id.score);
+        countdownText = (TextView) findViewById(R.id.countdownText);
+        pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+
+
+
+        answer_buttons = new Button[4];
+        answer_buttons[0] = button1;
+        answer_buttons[1] = button2;
+        answer_buttons[2] = button3;
+        answer_buttons[3] = button4;
+
+        //Clear All Button Text
+        for(int i = 0; i < answer_buttons.length; i++){
+            answer_buttons[i].setText("");
+        }
+
+        loadGame();
 
         // Create Contact when clicking on the image
-        final String contactName = rand_img;
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
                 intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
-                intent.putExtra(ContactsContract.Intents.Insert.NAME, contactName);
+                intent.putExtra(ContactsContract.Intents.Insert.NAME, rand_img);
                 timer.cancel();
                 if (timer2 != null){
                     timer2.cancel();
@@ -177,7 +203,7 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String buttonText = button1.getText().toString();
-                if (!buttonText.equalsIgnoreCase(contactName)){
+                if (!buttonText.equalsIgnoreCase(rand_img)){
                     Toast.makeText(getApplicationContext(), "Wrong!", Toast.LENGTH_SHORT).show();
                 } else {
                     // SharedPrefs Object to save score
@@ -187,8 +213,7 @@ public class GameActivity extends AppCompatActivity {
                     editor.apply(); //saving the changes
                 }
                 timer.cancel();
-                finish();
-                startActivity(getIntent());
+                loadGame();
             }
         });
 
@@ -196,7 +221,7 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String buttonText = button2.getText().toString();
-                if (!buttonText.equalsIgnoreCase(contactName)){
+                if (!buttonText.equalsIgnoreCase(rand_img)){
                     Toast.makeText(getApplicationContext(), "Wrong!", Toast.LENGTH_SHORT).show();
                 } else {
                     // SharedPrefs Object to save score
@@ -206,8 +231,7 @@ public class GameActivity extends AppCompatActivity {
                     editor.apply(); //saving the changes
                 }
                 timer.cancel();
-                finish();
-                startActivity(getIntent());
+                loadGame();
             }
         });
 
@@ -215,7 +239,7 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String buttonText = button3.getText().toString();
-                if (!buttonText.equalsIgnoreCase(contactName)){
+                if (!buttonText.equalsIgnoreCase(rand_img)){
                     Toast.makeText(getApplicationContext(), "Wrong!", Toast.LENGTH_SHORT).show();
                 } else {
                     // SharedPrefs Object to save score
@@ -225,8 +249,7 @@ public class GameActivity extends AppCompatActivity {
                     editor.apply(); //saving the changes
                 }
                 timer.cancel();
-                finish();
-                startActivity(getIntent());
+                loadGame();
             }
         });
 
@@ -234,7 +257,7 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String buttonText = button4.getText().toString();
-                if (!buttonText.equalsIgnoreCase(contactName)){
+                if (!buttonText.equalsIgnoreCase(rand_img)){
                     Toast.makeText(getApplicationContext(), "Wrong!", Toast.LENGTH_SHORT).show();
                 } else {
                     // SharedPrefs Object to save score
@@ -244,8 +267,7 @@ public class GameActivity extends AppCompatActivity {
                     editor.apply(); //saving the changes
                 }
                 timer.cancel();
-                finish();
-                startActivity(getIntent());
+                loadGame();
             }
         });
 
@@ -289,18 +311,6 @@ public class GameActivity extends AppCompatActivity {
     @Override
     public void onRestart(){
         super.onRestart();
-        timer2 = new CountDownTimer(5500, 1000) {
-            public void onTick(long millisUntilFinished) {
-                countdownText.setText("seconds remaining: " + millisUntilFinished / 1000);
-            }
-
-            public void onFinish() {
-                countdownText.setText("Times Up!");
-                if (countdownText.getText().toString().equals("Times Up!")) {
-                    finish();
-                    startActivity(getIntent());
-                }
-            }
-        }.start();
+        timer.start();
     }
 }
--- app/src/main/java/com/example/eric/memberquiz/MainActivity.java
+++ app/src/main/java/com/example/eric/memberquiz/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.eric.memberquiz;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
